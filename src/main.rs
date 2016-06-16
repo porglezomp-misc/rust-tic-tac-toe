63679be349d77d40c3deeeacd6f5ac45415ba138
Caleb Jones <self@calebjones.net>
2016-06-16 00:34:25 -0400

    Implement indexing for the Board

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+#![feature(range_contains)]
+
+use std::ops::{Index, IndexMut};
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Piece {
     X,
     O,
 }
+use Piece::*;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct Board {
@@ -17,7 +22,29 @@ impl Board {
     }
 }
 
+impl Index<(usize, usize)> for Board {
+    type Output = Option<Piece>;
+
+    fn index(&self, (row, col): (usize, usize)) -> &Option<Piece> {
+        if (0..3).contains(row) && (0..3).contains(col) {
+            &self.squares[row * 3 + col]
+        } else {
+            panic!("{},{} out of range for a tic-tac-toe board", row, col)
+        }
+    }
+}
+
+impl IndexMut<(usize, usize)> for Board {
+    fn index_mut(&mut self, (row, col): (usize, usize)) -> &mut Option<Piece> {
+        if (0..3).contains(row) && (0..3).contains(col) {
+            &mut self.squares[row * 3 + col]
+        } else {
+            panic!("{},{} out of range for a tic-tac-toe board", row, col)
+        }
+    }
+}
+
 fn main() {
-    let board = Board::new();
-    let _ = board;
+    let mut board = Board::new();
+    board[(1, 1)] = Some(X);
 }
